ad5caf49f7d59b268ffb5f37ab5514c42fcfce45
jeonguihyeong <jwh4861230@likelion.org>
2021-07-09 10:02:54 +0900

    7/9 M P commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class newModel(models.Model):  # 장고에서 제공해주는 기본 모델 ( models.Model)
+    text = models.CharField(max_length=255, null=False)  # 문자열을 입력받는 것을 만든다. max_length는 최대 문자열 길이를 설정
