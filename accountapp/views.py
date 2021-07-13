58d621f4b2dcf9020e388dace1937b61ce7745ff
jeonguihyeong <jwh4861230@likelion.org>
2021-07-13 10:24:10 +0900

    7/13 M P commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import newModel
+
+
 def hello_cat(request):
-    if request == 'POST':
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'}) #alt enter를 통해 import를 해줄수 있음
+    if request.method == 'POST':
+
+        temp = request.POST.get('input_text')
+
+        model_instance = newModel()
+        model_instance.text = temp
+        model_instance.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_cat'))
+
+
     else:
-        return render(request, 'accountapp/hello_world.html', context= {'text':'get method'})
+
+        data_list = newModel.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
