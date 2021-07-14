ff0b7c6c94e0638c73b2db83f7ad8cb85c1f0dea
jeonguihyeong <jwh4861230@likelion.org>
2021-07-14 16:16:17 +0900

    7/14 M P commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_cat, AccountCreateView
@@ -6,5 +7,11 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_cat/', hello_cat, name='hello_cat'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),  # loginview는 장고에서 제공해주는 클래스
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     path('create/', AccountCreateView.as_view(), name='create')
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -32,4 +32,4 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_cat')
-    template_name = 'account/create.html'
+    template_name = 'accountapp/create.html'
