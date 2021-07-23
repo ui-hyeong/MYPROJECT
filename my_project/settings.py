d277ee94926602439400a308457137a69e36342d
jeonguihyeong <jwh4861230@likelion.org>
2021-07-23 10:04:19 +0900

    7/23 M P commit 2

--- my_project/settings.py
+++ my_project/settings.py
@@ -137,10 +137,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
