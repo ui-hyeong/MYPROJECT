b0b014efbe4743227f2ac71c5c1c1bb38b8172fa
jeonguihyeong <jwh4861230@likelion.org>
2021-07-30 09:24:58 +0900

    7/30 M P commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -51,8 +51,11 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_cat')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+
 
 @method_decorator(has_ownership, 'get')  #함수에 쓰일 데코레이터를 메서드에도 적용시킨다. login을 어디에 적용시킬지도 작성해야한다.
 @method_decorator(has_ownership, 'post')
