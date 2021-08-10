2afe868ab4739fb653f80bfe311a393ceedff732
jeonguihyeong <jwh4861230@likelion.org>
2021-08-10 10:10:57 +0900

    8 / 10 M P commit 3

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def get_success_url(self): # 댓글 작성후에 이동하는 주소
+        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
