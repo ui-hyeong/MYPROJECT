f33646f5a77bd789d446e3870e6ec50d3b5c7f27
jeonguihyeong <jwh4861230@likelion.org>
2021-08-20 15:12:21 +0900

    8 / 20 M P commit 2

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from subscribeapp.views import SubscriptionView
+
+urlpatterns = [
+path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 ]
