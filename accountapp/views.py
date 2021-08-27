af8882193782f5cf24d0cfb141d4f9dee02e2163
jeonguihyeong <jwh4861230@likelion.org>
2021-08-27 09:20:20 +0900

    8 / 27 M P commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -15,29 +15,29 @@ from accountapp.forms import AccountCreationForm
 from accountapp.models import newModel
 from articleapp.models import Article
 
-
-def hello_cat(request):
-    if request.method == 'POST':
-
-        temp = request.POST.get('input_text')
-
-        model_instance = newModel()
-        model_instance.text = temp
-        model_instance.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_cat'))  # 주소를 안쓰고 라우팅을 헤서 연결할 수있다.
-
-
-    else:
-
-        data_list = newModel.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+#
+# def hello_cat(request):
+#     if request.method == 'POST':
+#
+#         temp = request.POST.get('input_text')
+#
+#         model_instance = newModel()
+#         model_instance.text = temp
+#         model_instance.save()
+#
+#         return HttpResponseRedirect(reverse('accountapp:hello_cat'))  # 주소를 안쓰고 라우팅을 헤서 연결할 수있다.
+#
+#
+#     else:
+#
+#         data_list = newModel.objects.all()
+#         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
 
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_cat')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -71,5 +71,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_cat')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
